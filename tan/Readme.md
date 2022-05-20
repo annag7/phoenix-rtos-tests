688748bb13a7ccf1c8f57638e40ff98ef94de4ed
AnnaG <38631187+annag7@users.noreply.github.com>
2022-05-20 11:11:00 +0200

    tan() function tests, readme added

---
+++ tan/Readme.md
@@ -0,0 +1,9 @@
+﻿File tan.c contains tests for tan() function from standard C library (phoenix).
+
+Using the equivalence classes the set [-pi/2; pi/2] was tested as input data.
+There was used boundary values analysis, chosen input:
+	- limit values that are outside the proper input set (-pi/2 and pi/2)
+	- values that are inside the domain and are close enough to the limits that can be called 'boundary values' (-pi/2+10e-6 and pi/2-10e-6)
+
+To make the test complete the middle values from the input set were also tested.
+Tasts were executed with set precision of 10e-12.
--- tan/tan.c
+++ tan/tan.c
@@ -9,46 +9,46 @@ TEST_GROUP(test_tan);
 TEST_SETUP(test_tan)
 {
 }
-
+ 
 TEST_TEAR_DOWN(test_tan)
 {
 }
 
 TEST(test_tan, tan_boundary_values_left_0)
 {
-	(tan(-M_PI_2) != tan(-M_PI_2)) ? PASS("") : FAIL("");
+	(tan(-M_PI_2) != tan(-M_PI_2)) ? PASS("test passed for input outside the domain") : FAIL("test failed for input outside the domain");
 }
 
 TEST(test_tan, tan_boundary_values_left_1)
 {
-	
+	TEST_ASSERT_EQUAL_DOUBLE(999999.999999666667, tan(-M_PI_2 + 10e-6));
 }
 
 TEST(test_tan, tan_boundary_values_right_0)
 {
-	(tan(M_PI_2) != tan(M_PI_2)) ? PASS("") : FAIL("");
+	(tan(M_PI_2) != tan(M_PI_2)) ? PASS("test passed for input outside the domain") : FAIL("test failed for input outside the domain");
 }
 
 TEST(test_tan, tan_boundary_values_right_1)
 {
-	
+	TEST_ASSERT_EQUAL_DOUBLE(999999.999999666667, tan(M_PI_2 - 10e-6));
 }
 
 
 TEST(test_tan, tan_middle_values)
 {
+	TEST_ASSERT_EQUAL_DOUBLE(-1.0, tan(-M_PI_4));
 	TEST_ASSERT_EQUAL_DOUBLE(0.0, tan(0));
-}
-
-TEST(test_tan, tan_improper_data_type)
-{
-	
+	TEST_ASSERT_EQUAL_DOUBLE(1.732050807569, tan(M_PI / 3));
 }
 
 
 TEST_GROUP_RUNNER(test_tan)
 {
 	RUN_TEST_CASE(test_tan, tan_boundary_values_left_0);
+	RUN_TEST_CASE(test_tan, tan_boundary_values_left_1);
+	RUN_TEST_CASE(test_tan, tan_boundary_values_right_0);
+	RUN_TEST_CASE(test_tan, tan_boundary_values_right_1);
 	RUN_TEST_CASE(test_tan, tan_middle_values);
 }
 
